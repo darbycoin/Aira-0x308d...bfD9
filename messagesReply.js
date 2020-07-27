a88f31139b6d128210c5b13beedaa06852ba0897
Sergei Lonshakov <sergeylonshakov@gmail.com>
2020-07-27 14:20:36 +0400

    New tag circ, price, cap

--- messagesReply.js
+++ messagesReply.js
@@ -5,4 +5,7 @@ module.exports = {
 2. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)
 3. [theoretical and practical information about Robonomics Token (XRT)](https://robonomics.network/community#token)
 4. [Distribution Release 2020](https://ipfs.io/ipfs/QmTPuS1DZtZSsjtBiPDLp4dqXaNs9P4JAR6kaiTG51sceG/Robonomics_token_distribution_release_May_2020.pdf)`,
+  "circ": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`,
+  "price": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`,
+  "cap": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`
 };

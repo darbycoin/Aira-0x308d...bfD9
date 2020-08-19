f5d192175dc320074083c5cca9097f694e09d409
Sergei Lonshakov <sergeylonshakov@gmail.com>
2020-08-19 11:39:13 +0400

    Roadmap, plans, Polkadot, Kusama, parachain tags

--- messagesReply.js
+++ messagesReply.js
@@ -1,11 +1,49 @@
 module.exports = {
-  release: `Read it in the recommended sequence
+  "tokenomics": `The information about following topics #tokenomics are in the recommended sequence:
 
 1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
 2. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)
 3. [theoretical and practical information about Robonomics Token (XRT)](https://robonomics.network/community#token)
 4. [Distribution Release 2020](https://ipfs.io/ipfs/QmTPuS1DZtZSsjtBiPDLp4dqXaNs9P4JAR6kaiTG51sceG/Robonomics_token_distribution_release_May_2020.pdf)`,
+  
   "circ": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`,
+ 
   "price": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`,
-  "cap": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`
+  
+  "cap": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`,
+  
+  "roadmap": `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
+
+1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
+2. [On top of two world computers. Research questions](https://blog.aira.life/on-top-of-two-world-computers-research-questions-a83570f4c8b1)
+3. [Robonomics parachain on Kusama](https://blog.aira.life/robonomics-parachain-on-kusama-40853780c709)
+4. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)`,
+   
+   "parachain": `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
+  
+1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
+2. [On top of two world computers. Research questions](https://blog.aira.life/on-top-of-two-world-computers-research-questions-a83570f4c8b1)
+3. [Robonomics parachain on Kusama](https://blog.aira.life/robonomics-parachain-on-kusama-40853780c709)
+4. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)`,
+  
+     "Kusama": `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
+  
+1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
+2. [On top of two world computers. Research questions](https://blog.aira.life/on-top-of-two-world-computers-research-questions-a83570f4c8b1)
+3. [Robonomics parachain on Kusama](https://blog.aira.life/robonomics-parachain-on-kusama-40853780c709)
+4. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)`,
+  
+     "Polkadot": `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
+  
+1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
+2. [On top of two world computers. Research questions](https://blog.aira.life/on-top-of-two-world-computers-research-questions-a83570f4c8b1)
+3. [Robonomics parachain on Kusama](https://blog.aira.life/robonomics-parachain-on-kusama-40853780c709)
+4. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)`,
+  
+     "plans": `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
+  
+1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
+2. [On top of two world computers. Research questions](https://blog.aira.life/on-top-of-two-world-computers-research-questions-a83570f4c8b1)
+3. [Robonomics parachain on Kusama](https://blog.aira.life/robonomics-parachain-on-kusama-40853780c709)
+4. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)`,
 };

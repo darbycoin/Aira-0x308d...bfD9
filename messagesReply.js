42c4a1a6f7dd868fcf1e2742a1378f13f26af3d0
vol4tim <vol4tim@gmail.com>
2020-08-19 13:40:10 +0400

    upd messages reply

--- messagesReply.js
+++ messagesReply.js
@@ -1,51 +1,24 @@
-module.exports = {
-  "tokenomics": `The information about following topics #tokenomics are in the recommended sequence:
+module.exports = [
+  {
+    words: ['tokenomics'],
+    message: `The information about following topics #tokenomics are in the recommended sequence:
 
 1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
 2. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)
 3. [theoretical and practical information about Robonomics Token (XRT)](https://robonomics.network/community#token)
-4. [Distribution Release 2020](https://ipfs.io/ipfs/QmTPuS1DZtZSsjtBiPDLp4dqXaNs9P4JAR6kaiTG51sceG/Robonomics_token_distribution_release_May_2020.pdf)`,
-  
-  "circ": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`,
- 
-  "price": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`,
-  
-  "cap": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`,
-  
-  "trading": `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`,
-  
-  "roadmap": `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
+4. [Distribution Release 2020](https://ipfs.io/ipfs/QmTPuS1DZtZSsjtBiPDLp4dqXaNs9P4JAR6kaiTG51sceG/Robonomics_token_distribution_release_May_2020.pdf)`
+  },
+  {
+    words: ['circ','price','cap', 'trading'],
+    message: `Community! price, circ, cap discussion here [Trade XRT](https://t.me/joinchat/A6wE1FiSPStvVN4MKmZDyg)`
+  },
+  {
+    words: ['roadmap','parachain','Kusama', 'Polkadot', 'plans'],
+    message: `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
 
 1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
 2. [On top of two world computers. Research questions](https://blog.aira.life/on-top-of-two-world-computers-research-questions-a83570f4c8b1)
 3. [Robonomics parachain on Kusama](https://blog.aira.life/robonomics-parachain-on-kusama-40853780c709)
 4. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)`,
-   
-   "parachain": `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
-  
-1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
-2. [On top of two world computers. Research questions](https://blog.aira.life/on-top-of-two-world-computers-research-questions-a83570f4c8b1)
-3. [Robonomics parachain on Kusama](https://blog.aira.life/robonomics-parachain-on-kusama-40853780c709)
-4. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)`,
-  
-     "Kusama": `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
-  
-1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
-2. [On top of two world computers. Research questions](https://blog.aira.life/on-top-of-two-world-computers-research-questions-a83570f4c8b1)
-3. [Robonomics parachain on Kusama](https://blog.aira.life/robonomics-parachain-on-kusama-40853780c709)
-4. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)`,
-  
-     "Polkadot": `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
-  
-1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
-2. [On top of two world computers. Research questions](https://blog.aira.life/on-top-of-two-world-computers-research-questions-a83570f4c8b1)
-3. [Robonomics parachain on Kusama](https://blog.aira.life/robonomics-parachain-on-kusama-40853780c709)
-4. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)`,
-  
-     "plans": `The information about following topics #roadmap, #parachain, #Kusama, #Polkadot are in the recommended sequence:
-  
-1. [Infrastructure && tech development timeline](https://robonomics.network/timeline)
-2. [On top of two world computers. Research questions](https://blog.aira.life/on-top-of-two-world-computers-research-questions-a83570f4c8b1)
-3. [Robonomics parachain on Kusama](https://blog.aira.life/robonomics-parachain-on-kusama-40853780c709)
-4. [Foundation of the Robonomics parachain](https://blog.aira.life/foundation-of-the-robonomics-parachain-390dfac09e5d)`,
-};
+  }
+];
--- src/index.js
+++ src/index.js
@@ -47,13 +47,21 @@ email.on("message", (ctx) => ctx.reply("Invalid email"));
 const bot = new Telegraf(config.TOKEN);
 bot.start((ctx) => ctx.reply("Welcome"));
 
-const words = Object.keys(messagesReply);
+const dict = messagesReply.reduce((accumulator, item, index) => {
+  item.words.forEach(word => {
+    accumulator[word] = index
+  });
+  return accumulator;
+}, {});
+const words = Object.keys(dict);
+
 bot.on("text", async (ctx, next) => {
+  // console.log(ctx.update, ctx.botInfo);
   const i = words.findIndex((item) => {
     return ctx.message.text.search(new RegExp(item, "i")) >= 0;
   });
   if (i >= 0) {
-    await ctx.replyWithMarkdown(messagesReply[words[i]], {
+    await ctx.replyWithMarkdown(messagesReply[dict[words[i]]].message, {
       reply_to_message_id: ctx.message.message_id,
     });
   }
@@ -64,5 +72,13 @@ const stage = new Stage([issue, email], { ttl: 200 });
 bot.use(session());
 bot.use(stage.middleware());
 bot.command("issue", (ctx) => ctx.scene.enter("issue"));
+
+bot.on("new_chat_members", (ctx) => {
+  let members = "";
+  ctx.message.new_chat_members.forEach((member) => {
+    members += "@" + member.username + " ";
+  });
+  ctx.reply(members + "привет");
+});
 
 bot.launch();
